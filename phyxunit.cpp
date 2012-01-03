3c51509717b0c968f003ff99cd57b4b6db28a51a
Alexander Rössler <mail.aroessler@gmail.com>
2012-01-03 12:21:57 +0000

    some work on the units

--- phyxunit.cpp
+++ phyxunit.cpp
@@ -3,9 +3,131 @@
 PhyxUnit::PhyxUnit(QObject *parent) :
     QObject(parent)
 {
+    m_symbol = "";
+    m_name   = "";
+    m_type   = BaseUnitType;
+    m_offset = 0;
+    m_scaleFactor = 1;
+    m_prefixPower = 0;
+    m_flags = 0;
 }
 
-void PhyxUnit::addPower(QString base, double power)
+void PhyxUnit::appendPower(QString base, double power)
 {
-    powers.insert(base, power);
+    m_powers.insert(base, power);
+}
+
+void PhyxUnit::powerMultiply(QString base, double factor)
+{
+    double power;
+    if (m_powers.contains(base))
+    {
+        power = m_powers.value(base);
+        power += factor;
+    }
+    else
+    {
+        power = factor;
+    }
+
+    if (power == 0)
+        m_powers.remove(base);
+    else
+        m_powers.insert(base, power);
+}
+
+void PhyxUnit::powerDevide(QString base, double factor)
+{
+    double power;
+    if (m_powers.contains(base))
+    {
+        power = m_powers.value(base);
+        power -= factor;
+    }
+    else
+    {
+        power = factor;
+    }
+
+    if (power == 0)
+        m_powers.remove(base);
+    else
+        m_powers.insert(base, power);
+}
+
+void PhyxUnit::powersMultiply(PhyxUnit::PowerMap powers)
+{
+    QMapIterator<QString, double> i(powers);
+     while (i.hasNext()) {
+         i.next();
+         powerMultiply(i.key(), i.value());
+     }
+}
+
+void PhyxUnit::powersDevide(PhyxUnit::PowerMap powers)
+{
+    QMapIterator<QString, double> i(powers);
+     while (i.hasNext()) {
+         i.next();
+         powerDevide(i.key(), i.value());
+     }
+}
+
+void PhyxUnit::powersRaise(double power)
+{
+    QMapIterator<QString, double> i(m_powers);
+     while (i.hasNext()) {
+         i.next();
+         m_powers.insert(i.key(), i.value() * power);
+     }
+}
+
+void PhyxUnit::powersRoot(double root)
+{
+    QMapIterator<QString, double> i(m_powers);
+     while (i.hasNext()) {
+         i.next();
+         m_powers.insert(i.key(), i.value() / root);
+     }
+}
+
+bool PhyxUnit::powersCompare(PhyxUnit::PowerMap powers)
+{
+    return m_powers == powers;
+}
+
+void PhyxUnit::prefixMultiply(double factor)
+{
+    m_prefixPower += factor;
+}
+
+void PhyxUnit::prefixDevide(double factor)
+{
+    m_prefixPower -= factor;
+}
+
+void PhyxUnit::prefixRaise(double power)
+{
+    m_prefixPower *= power;
+}
+
+void PhyxUnit::prefixRoot(double root)
+{
+    m_prefixPower /= root;
+}
+
+bool PhyxUnit::isDimensionless()
+{
+    return m_powers.isEmpty();
+}
+
+bool PhyxUnit::operator ==(PhyxUnit unit)
+{
+    if (type() != unit.type())
+        return false;
+    if (offset() != unit.offset())
+        return false;
+    if (scaleFactor() != unit.scaleFactor())
+        return false;
+    return powersCompare(unit.powers());
 }
--- phyxunit.h
+++ phyxunit.h
@@ -12,6 +12,8 @@ class PhyxUnit : public QObject
     Q_PROPERTY(UnitType type READ type WRITE setType)
     Q_PROPERTY(double offset READ offset WRITE setOffset)
     Q_PROPERTY(double scaleFactor READ scaleFactor WRITE setScaleFactor)
+    Q_PROPERTY(PowerMap powers READ powers WRITE setPowers)
+    Q_PROPERTY(double prefixPower READ prefixPower WRITE setPrefixPower)
     Q_PROPERTY(UnitFlags flags READ flags WRITE setFlags)
     Q_ENUMS(UnitType)
     Q_FLAGS(UnitFlag UnitFlags)
@@ -30,47 +32,73 @@ public:
 
     /** Flags for unit handling*/
     enum UnitFlag {
-        SiUnitFlag = 0x01,         /// flag for Si-Units, needed to handle Si-Prefixes
-        TimeUnitFlag = 0x02        /// flag for time units (min, h, d), to avoid Si-Prefixes beeing used with them
+        SiUnitFlag = 0x01         /// flag for Si-Units, needed to handle Si-Prefixes
+    //    TimeUnitFlag = 0x02        /// flag for time units (min, h, d), to avoid Si-Prefixes beeing used with them
     };
     Q_DECLARE_FLAGS(UnitFlags, UnitFlag)
 
-    void addPower(QString base, double power);
+    typedef QMap<QString, double> PowerMap;
 
-    QString symbol() const
+    void appendPower(QString base, double power);       /// adds a power to the map
+    void powerMultiply(QString base, double factor);    /// multiplies a power with factor
+    void powerDevide(QString base, double factor);      /// devides a power with factor
+    void powersMultiply(PowerMap powers);               /// multiplies powers of the unit with other powers
+    void powersDevide(PowerMap powers);                 /// devides powers of the unit with other powers
+    void powersRaise(double power);                     /// raises all powers to power
+    void powersRoot(double root);                       /// takes the root of all powers
+    bool powersCompare(PowerMap powers);                /// compares powers of the unit with oder powers and returns ==
+
+    void prefixMultiply(double factor);
+    void prefixDevide(double factor);
+    void prefixRaise(double power);
+    void prefixRoot(double root);
+
+    bool isDimensionless();                             /// returns wheter the unit is dimensionless or not
+
+    bool operator ==(PhyxUnit unit);
+
+    QString     symbol() const
     {
         return m_symbol;
     }
-    QString name() const
+    QString     name() const
     {
         return m_name;
     }
-    UnitType type() const
+    UnitType    type() const
     {
         return m_type;
     }
-    double offset() const
+    double      offset() const
     {
         return m_offset;
     }
-    double scaleFactor() const
+    double      scaleFactor() const
     {
         return m_scaleFactor;
     }
-    UnitFlags flags() const
+    UnitFlags   flags() const
     {
         return m_flags;
     }
+    PowerMap powers() const
+    {
+        return m_powers;
+    }
+    double prefixPower() const
+    {
+        return m_prefixPower;
+    }
 
 private:
-    QMap<QString, double>   powers;     /// this map holds the powers of the base units
-    
-    QString m_symbol;                   /// the symbol of the unit
-    QString m_name;                     /// the name of the unit
-    UnitType m_type;                    /// the type of the unit
-    double m_offset;                    /// offset for OffsetUnit and GalileanUnit
-    double m_scaleFactor;               /// scale factor for GalileanUnit and LogarithmicUnit
-    UnitFlags m_flags;                  /// flags of the unit
+    QString     m_symbol;                  /// the symbol of the unit
+    QString     m_name;                    /// the name of the unit
+    UnitType    m_type;                    /// the type of the unit
+    double      m_offset;                  /// offset for OffsetUnit and GalileanUnit
+    double      m_scaleFactor;             /// scale factor for GalileanUnit and LogarithmicUnit
+    PowerMap    m_powers;                  /// this map holds the powers of the base units
+    double      m_prefixPower;             /// for Si units -> the power of the prefix (10^x)
+    UnitFlags   m_flags;                   /// flags of the unit
 
 signals:
     
@@ -99,6 +127,14 @@ void setScaleFactor(double arg)
 void setFlags(UnitFlags arg)
 {
     m_flags = arg;
+}
+void setPowers(PowerMap arg)
+{
+    m_powers = arg;
+}
+void setPrefixPower(double arg)
+{
+    m_prefixPower = arg;
 }
 };
 
